0c724c8698d67a73bb665ab8b0736983a30fff50
Ayange Felix <ayangefelix8@gmail.com>
2022-12-12 23:01:18 +0100

    add todo model

---
+++ models/todoModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const TodoSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a title'],
+      maxlength: [100, 'Title can not be more than 100 characters'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide a description'],
+      maxlength: [1000, 'Description can not be more than 1000 characters'],
+    },
+  },
+  { timestamps: true },
+)
+
+module.exports = mongoose.model('Todo', TodoSchema)
