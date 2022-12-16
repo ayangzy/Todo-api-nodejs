e7e739b0468ddd2225ac51ad289c8e759d41dae1
Ayange Felix <ayangefelix8@gmail.com>
2022-12-16 09:21:10 +0100

    fix server error issue

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(bodyParser.json())
 
-app.use('/', (req, res) => {
-  res.status(200).send({
-    status: 200,
-    message: 'Welcome to TodoAPI',
-  })
-})
+// app.use('/', (req, res) => {
+//   res.status(200).send({
+//     status: 200,
+//     message: 'Welcome to TodoAPI',
+//   })
+// })
 
 app.use('/api/v1/todos', todoRoutes)
 app.use('/api/v1/generate-jwt-token', jwtToken)
